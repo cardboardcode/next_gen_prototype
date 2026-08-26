851db83ff7d0ba094cdf345b1a6c257e9c9fbf1b
Gary Bey <beyhy94@gmail.com>
2026-08-26 22:59:25 +0800

    :hammer: Updated to latest.
    Signed-off-by: Gary Bey <beyhy94@gmail.com>

---
+++ scripts/10_send_test_goal_robot0.bash
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+docker exec ros2_rust_nav2_tb4_sim_c bash -c "source /nav2_traffic_ws/install/setup.bash && ros2 action send_goal robot0/navigate_to_pose nav2_msgs/action/NavigateToPose \"{
+      pose: {
+        header: {
+          stamp: {sec: 0, nanosec: 0},
+          frame_id: 'map'
+        },
+        pose: {
+          position: {x: -0.558153510093689, y: 2.712405293062329292, z: 0.0},
+          orientation: {x: 0.0, y: 0.0, z: 0.0, w: 1.0}
+        }
+      }
+    }\"
+"
