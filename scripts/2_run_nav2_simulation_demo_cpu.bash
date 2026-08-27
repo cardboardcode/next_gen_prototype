93892bf3383458862fd0b31ac4981d1496370a4c
Gary Bey <beyhy94@gmail.com>
2026-08-27 16:03:58 +0800

    :hammer: Added CPU variant sim dev script run.
    Signed-off-by: Gary Bey <beyhy94@gmail.com>

---
+++ scripts/2_run_nav2_simulation_demo_cpu.bash
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+docker container stop ros2_rust_nav2_tb4_sim_c
+docker container rm ros2_rust_nav2_tb4_sim_c
+
+xhost +local:docker
+
+docker run -it --rm \
+--name ros2_rust_nav2_tb4_sim_c \
+--network host \
+--env="DISPLAY" \
+--env="QT_X11_NO_MITSHM=1" \
+--env CYCLONEDDS_URI=/ros2_ws/cyclonedds.xml \
+-e RMW_IMPLEMENTATION=rmw_zenoh_cpp \
+--device=/dev/dri \
+-v /tmp/.X11-unix:/tmp/.X11-unix \
+-v ./cyclonedds.xml:/ros2_ws/cyclonedds.xml \
+-v ./scripts/cloned_multi_tb3_simulation_launch.py:/nav2_traffic_ws/install/sp_demo_nav2_bringup/share/sp_demo_nav2_bringup/launch/cloned_multi_tb3_simulation_launch.py \
+-v ./scripts/demo_nav2.launch.py:/nav2_traffic_ws/install/rmf_path_server_demo/share/rmf_path_server_demo/launch/demo_nav2.launch.py \
+ros2_rust_nav2_tb4_sim:jazzy bash -c "source /nav2_traffic_ws/install/setup.bash && ros2 launch sp_demo_nav2_bringup cloned_multi_tb3_simulation_launch.py robots:=\"robot0={x: 0.0, y: 5.0, yaw: 0.0}; robot1={x: 3.0, y: 5.0, yaw: 0.0};\""
---
+++ scripts/4_run_nav2_simulation_custom_cpu.bash
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+docker container stop ros2_rust_nav2_tb4_sim_c
+docker container rm ros2_rust_nav2_tb4_sim_c
+
+xhost +local:docker
+
+docker run -it --rm \
+    --name ros2_rust_nav2_tb4_sim_c \
+    --network host \
+    --device /dev/dri \
+    --env DISPLAY="${DISPLAY}" \
+    --env QT_X11_NO_MITSHM=1 \
+    --env XDG_RUNTIME_DIR=/tmp/runtime-root \
+    --env CYCLONEDDS_URI=/ros2_ws/cyclonedds.xml \
+    --env RMW_IMPLEMENTATION=rmw_cyclonedds_cpp \
+    -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
+    -v ./scripts/assets/simple_map.sdf:/opt/ros/jazzy/share/nav2_minimal_tb4_sim/worlds/simple_map.sdf \
+    -v ./scripts/assets/simple_map.pgm:/nav2_traffic_ws/install/sp_demo_nav2_bringup/share/sp_demo_nav2_bringup/maps/simple_map.pgm \
+    -v ./scripts/assets/simple_map.yaml:/nav2_traffic_ws/install/sp_demo_nav2_bringup/share/sp_demo_nav2_bringup/maps/simple_map.yaml \
+    -v ./cyclonedds.xml:/ros2_ws/cyclonedds.xml \
+    -v ./scripts/custom_cloned_multi_tb3_simulation_launch.py:/nav2_traffic_ws/install/sp_demo_nav2_bringup/share/sp_demo_nav2_bringup/launch/custom_cloned_multi_tb3_simulation_launch.py \
+    -v "./scripts/assets/gcache:/root/.gz/fuel/fuel.gazebosim.org/openrobotics/models/" \
+ros2_rust_nav2_tb4_sim:jazzy \
+    bash -c "source /nav2_traffic_ws/install/setup.bash && \
+    ros2 launch sp_demo_nav2_bringup custom_cloned_multi_tb3_simulation_launch.py \
+    robots:=\"robot0={x: -0.558153510093689, y: -1.112405293062329292, yaw: 0.0}; \
+    robot1={x: -0.558153510093689, y: 0.712405293062329292, yaw: 0.0};\""
